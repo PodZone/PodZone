f84d849b3da7ccb82e3d116d98bac64f24c7d9d1
Bardyl <bardyl@menutmathieu.fr>
2013-11-12 19:58:42 +0100

    [Master] Left sidebar transitions && homepage bugfix

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -7,6 +7,12 @@ $(function(){
 		$('#filter').removeClass('over');	
 		// Display sidebar
 		$('#left-sidebar').toggleClass('open');
+		if($('#filter').is('.filterBlack')){
+			$('#filter').on('click', function(){
+				$('#filter').removeClass('filterBlack');
+				$('#left-sidebar').removeClass('open');
+			});
+		}
 	});
 
 	$('.menu-button').on('mouseenter', function(){
