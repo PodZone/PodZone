0f727684af020b86f09be13774262b129069b38b
Bardyl <bardyl@menutmathieu.fr>
2013-11-13 18:28:43 +0100

    Sidebar && first podcasts page

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,20 +1,53 @@
-$(function(){
+var player = {
 
-	// Event on play pause button to start/pause mp3 file
-	$('#playpause').on('click', function()
+	params: {
+		media: '#player',
+		buttons: {
+			playPause: '#playpause',
+			prev: '#prev',
+			next: '#next'
+		}
+	},
+
+	init: function(options)
 	{
-		if($(this).is('.play'))
+		this.params = $.extend(this.params, options);
+	},
+
+	playpause: function()
+	{
+		console.log($(player.params.buttons.playPause).classList);
+		if($(player.params.buttons.playPause).is('.play'))
 		{
-			$('audio').get(0).play();
-			$(this).attr('src', '<?php echo $img_dir; ?>pause.png');
-			$(this).attr('class', 'pause');
+			document.querySelector(player.params.media).play();
 		}
 		else
 		{
-			$('audio').get(0).pause();
-			$(this).attr('src', '<?php echo $img_dir; ?>play.png');
-			$(this).attr('class', 'play');
+			document.querySelector(player.params.media).pause();
 		}
-	});
+		
+	},
+}
 
-});
+
+
+// $(function(){
+
+// 	// Event on play pause button to start/pause mp3 file
+// 	$('#playpause').on('click', function()
+// 	{
+// 		if($(this).is('.play'))
+// 		{
+// 			$('audio').get(0).play();
+// 			$(this).attr('src', '<?php echo $img_dir; ?>pause.png');
+// 			$(this).attr('class', 'pause');
+// 		}
+// 		else
+// 		{
+// 			$('audio').get(0).pause();
+// 			$(this).attr('src', '<?php echo $img_dir; ?>play.png');
+// 			$(this).attr('class', 'play');
+// 		}
+// 	});
+
+// });
